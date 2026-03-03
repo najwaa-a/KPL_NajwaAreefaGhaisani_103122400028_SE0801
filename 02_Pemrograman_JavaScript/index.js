799a8f74eccddb78a674a1e9aa2410310962815c
najwaa-a <najwaareefa@gmail.com>
2026-03-03 22:12:49 +0700

    feat: update data array and if else condition

--- 02_Pemrograman_JavaScript/index.js
+++ 02_Pemrograman_JavaScript/index.js
@@ -1,9 +1,9 @@
-const arr1 = [2, 26, 28, -2];
+const arr1 = [2, 0, 26, 28, -2];
 
-function mulOfArray(arr) {
+function mulOfArray(arr) { 
     let result = 1;
-    for (let i = 0; i < arr.length; i = i + 1) {
-        if (arr[i] >= 0) {
+    for (let i = 0; i < arr.length; i = i + 1) { 
+        if (arr[i] > 0) { 
             result = result * arr[i];
         }
     }
